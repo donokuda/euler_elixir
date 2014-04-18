e2c6c9ed134c264b5bdd3fee3fc785f986eecb6e
Don Okuda <don.okuda@gmail.com>
2014-04-18 16:02:32 -0700

    wip

--- lib/euler/problem_3.ex
+++ lib/euler/problem_3.ex
@@ -15,4 +15,49 @@ defmodule Euler.Problem3 do
   def the_answer do
 
   end
+
+  @doc """
+
+  Return a list of prime factors for an integer
+
+  ## Examples
+
+    iex> Euler.Problem3.prime_factors(48)
+    [2, 3]
+
+    iex> Euler.Problem3.prime_factors(13195)
+    [5, 7, 13, 29]
+
+  """
+  def prime_factors(integer) do
+    factor_tree(integer) |> Enum.uniq
+  end
+
+  @doc"""
+
+  Returns a factor tree for an integer
+
+  ## Example
+
+    iex> Euler.Problem3.factor_tree(48)
+    [2, 2, 2, 2, 3]
+
+  """
+  def factor_tree(integer) do
+    if Integer.even?(integer) do
+      factor_tree(integer, [2, div(integer, 2)])
+    end
+  end
+
+  def factor_tree(integer, array) do
+    last_num = List.last(array)
+    if Integer.even?(last_num) do
+      old_array = List.delete_at(array, -1)
+      new_array = old_array ++ [2, div(last_num, 2)]
+
+      factor_tree(List.last(new_array), new_array)
+    else
+      array
+    end
+  end
 end
